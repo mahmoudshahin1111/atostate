8cebe1b4eec6c69b508acd85fe8e0228f0068fc1
mahmoud shahin <mahmoudshahin1111@gmail.com>
2026-01-04 03:52:10 +0200

    install minifier + creating multi versions of the library to work for node and browser

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,50 @@
 const typescript = require('@rollup/plugin-typescript');
+const terser = require('@rollup/plugin-terser');
+const resolve = require('@rollup/plugin-node-resolve');
 
-module.exports = {
-	input: 'src/index.ts',
-	output: {
-		dir: 'dist',
-		format: 'cjs'
-	},
-	plugins:[typescript()]
-};
+const plugins = [resolve(), typescript({ tsconfig: './tsconfig.json' })];
+
+module.exports = [
+  // =========================
+  // Browser builds
+  // =========================
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.browser.esm.js',
+        format: 'es',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.browser.iife.js',
+        format: 'iife',
+        name: 'atostate',
+        sourcemap: true,
+        plugins: [terser()],
+      },
+    ],
+    plugins,
+  },
+
+  // =========================
+  // Node.js builds
+  // =========================
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: 'dist/index.node.esm.js',
+        format: 'es',
+        sourcemap: true,
+      },
+      {
+        file: 'dist/index.node.cjs',
+        format: 'cjs',
+        exports: 'named',
+        sourcemap: true,
+      },
+    ],
+    plugins,
+  },
+];
